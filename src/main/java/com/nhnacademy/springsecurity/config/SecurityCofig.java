977d2d66fc0ef4f2546099aa85ad2b7d7bc995d9
suebin <suuebin@gmail.com>
2023-05-16 21:31:55 +0900

    add SecurityConfig

--- src/main/java/com/nhnacademy/springsecurity/config/SecurityCofig.java
+++ src/main/java/com/nhnacademy/springsecurity/config/SecurityCofig.java
@@ -7,8 +7,14 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -29,6 +35,10 @@ public class SecurityCofig {
                     .requestMatchers("/private-project/**").requiresSecure()
                     .requestMatchers("/project/**").requiresSecure()
                     .anyRequest().requiresInsecure()
+                    .and()
+                .oauth2Login()
+                    .clientRegistrationRepository(clientRegistrationRepository())
+                    .authorizedClientService(authorizedClientService())
                     .and()
                 .formLogin()
                     .usernameParameter("id")
@@ -66,5 +76,23 @@ public class SecurityCofig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(github());
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
+
+    private ClientRegistration github() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .userNameAttributeName("name")
+                .clientId("cd01848c03d880143f1f")
+                .clientSecret("08fbb7a69cf992948e339ee320906cf1acd5687a")
+                .build();
     }
 }
